077bf1d885c259966a841b38d96ae47ef5f0874a
inayafaridah <inayaafaridah@gmail.com>
2024-12-12 09:22:22 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
 public class Nomor1 {
+    public static void angkaGanjil(int n) {
+        System.out.println("Tampilkan angka ganjil dari 0 hingga : " + n);
+        for (int i= 1; i <= n; i += 2) {
+            System.out.println(i + "");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan nilai n: ");
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+        
+        angkaGanjil(N);
     }
-    
 }
